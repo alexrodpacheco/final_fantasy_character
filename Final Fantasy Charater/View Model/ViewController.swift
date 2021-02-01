d8666209a8c729bdecab2a11b0697c935423ef98
Alex Pacheco <alexrodpacheco@gmail.com>
2021-02-01 18:33:46 -0300

    update ViewController

--- Final Fantasy Charater/View Model/ViewController.swift
+++ Final Fantasy Charater/View Model/ViewController.swift
@@ -7,22 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-  
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CharacterSucess, CharacterError {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     let characterViewModel = CharacterViewModel()
+    var character: [Character] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         characterViewModel.loadCharacter()
+        characterViewModel.sucessDelegate = self
+        characterViewModel.errorDelegate = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        return character.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CharacterCell", for: indexPath) as! CharacterCell
+        
+        //fetch cell
+        let name = character[indexPath.row].name
+        
+        //configure cell
+        cell.textLabel?.text = name
+        
+        return cell
+    }
+    
+    func characterSucess(sucess: [Character]) {
+        self.character = sucess
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+        
+    }
+    
+    func characterError(error: Error) {
+        print(error)
     }
 }
 
