d2df4946df71c160f714e96bf680f23318225e1c
Alex Pacheco <alexrodpacheco@gmail.com>
2021-02-01 18:35:04 -0300

    Update

--- Final Fantasy Charater/View Model/CharacterCell.swift
+++ Final Fantasy Charater/View Model/CharacterCell.swift
@@ -8,16 +8,9 @@
 import UIKit
 
 class CharacterCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
+    @IBOutlet weak var avatarName: UILabel!
+    @IBOutlet weak var avatarID: UILabel!
+    
 }
+
