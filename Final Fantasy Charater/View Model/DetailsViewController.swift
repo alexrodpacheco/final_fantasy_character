03b54637fa99dcf0c184ac1f2f6c0d1a72da6b3b
Alex Pacheco <alexrodpacheco@gmail.com>
2021-02-25 23:10:26 -0300

    Create and update DetailsViewController
    Create and update DetailsViewController for diplay DetailsClass data on the app screen when cell is selected

---
+++ Final Fantasy Charater/View Model/DetailsViewController.swift
@@ -0,0 +1,56 @@
+//
+//  DetailsViewController.swift
+//  Final Fantasy IX
+//
+//  Created by Alex Rodrigues Pacheco on 24/02/21.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController, DetailSucess, DetailError {
+    
+    @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var expLevel: UILabel!
+    @IBOutlet weak var expLevelMax: UILabel!
+    @IBOutlet weak var level: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let detailViewModel = DetailViewModel()
+        detailViewModel.sucessDelegate = self
+        detailViewModel.sucessError = self
+        detailViewModel.loadDetail()
+        
+        
+    }
+    
+    func detailsSucess(sucess: DetailsClass) {
+        
+        expLevel.text = String(sucess.activeClassJob.expLevel)
+        expLevelMax.text = String(sucess.activeClassJob.expLevelMax)
+        level.text = String(sucess.activeClassJob.level)
+        
+        guard let imgURL = URL(string: sucess.avatar) else {
+            //TODO: error
+            return
+        }
+        
+        do {
+            let imageData = try Data(contentsOf: imgURL)
+            guard let image: UIImage = UIImage(data: imageData) else {
+                return
+            }
+            self.avatarImage.image = image
+            
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    func detailError(error: Error) {
+        print(error)
+    }
+    
+}
