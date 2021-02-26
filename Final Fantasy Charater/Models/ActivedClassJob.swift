6bba5104da9f25e7b8f90e3bf21d33f3e2971cf7
Alex Pacheco <alexrodpacheco@gmail.com>
2021-02-25 23:04:37 -0300

    JSON object for data request
    JSON object for data request + Codingkeys + init decoder

---
+++ Final Fantasy Charater/Models/ActivedClassJob.swift
@@ -0,0 +1,24 @@
+//
+//  ClassJobs.swift
+//  Final Fantasy IX
+//
+//  Created by Alex Rodrigues Pacheco on 22/02/21.
+//
+
+import Foundation
+
+struct ActiveClassJob: Decodable {
+    let classID: Int
+    let expLevel: Int
+    let expLevelMax: Int
+    let level: Int
+    let name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case classID = "ClassID"
+        case expLevel = "ExpLevel"
+        case expLevelMax = "ExpLevelMax"
+        case level = "Level"
+        case name = "Name"
+    }
+}
---
+++ Final Fantasy Charater/Models/DetailsClass.swift
@@ -0,0 +1,29 @@
+//
+//  Details.swift
+//  Final Fantasy IX
+//
+//  Created by Alex Rodrigues Pacheco on 22/02/21.
+//
+
+import Foundation
+
+struct DetailsClass: Decodable {
+    let avatar: String
+    let bio: String
+    let activeClassJob: ActiveClassJob
+    
+    enum CodingKeys: String, CodingKey {
+            case character = "Character"
+            case avatar = "Avatar"
+            case bio = "Bio"
+            case activeClassJob = "ActiveClassJob"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let character = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .character)
+        avatar = try character.decode(String.self, forKey: .avatar)
+        bio = try character.decode(String.self, forKey: .bio)
+        activeClassJob = try character.decode(ActiveClassJob.self, forKey: .activeClassJob)
+    }
+}
