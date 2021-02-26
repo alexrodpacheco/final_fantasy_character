f1780722285cc8c7f74f706356fac14e7cdcc6d3
Alex Pacheco <alexrodpacheco@gmail.com>
2021-02-25 23:08:19 -0300

    Details Results

---
+++ Final Fantasy Charater/View Model/DetailViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewModel.swift
+//  Final Fantasy IX
+//
+//  Created by Alex Rodrigues Pacheco on 25/02/21.
+//
+
+import Foundation
+
+protocol DetailSucess {
+    func detailsSucess(sucess: DetailsClass)
+}
+
+protocol DetailError {
+    func detailError (error: Error)
+}
+
+class DetailViewModel {
+    
+    func loadDetail() {
+        let service = CharacterService()
+        service.getDetails { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let classJob):
+                    self.sucessDelegate?.detailsSucess(sucess: classJob)
+                case .failure(let error):
+                    self.sucessError?.detailError(error: error)
+                }
+            }
+            
+        }
+    }
+    
+    var sucessDelegate: DetailSucess?
+    var sucessError: DetailError?
+}
