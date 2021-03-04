e2c52de720ed5816c698c7525d7b013a19bb781e
Alex Pacheco <alexrodpacheco@gmail.com>
2021-03-04 18:04:30 -0300

    Paste Organizer

--- Final Fantasy Charater/Scenes/Character_Scene/CharacterViewController.swift
+++ Final Fantasy Charater/Scenes/Character_Scene/CharacterViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CharacterSucess, CharacterError {
+class CharacterViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CharacterSucess, CharacterError {
     
     @IBOutlet weak var tableView: UITableView!
     
